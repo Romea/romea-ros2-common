98aad7bc89c23a576c39c4e16782ee6a93e1ae89
jean.laneurit <jean.laneurit@inrae.fr>
2022-12-13 13:10:32 +0100

    bringup: refactoring of python module

--- romea_common_bringup/python/romea_common_bringup.py
+++ romea_common_bringup/python/romea_common_bringup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 
-def robot_prefix(robot_name):
+def robot_prefix(robot_namespace):
 
-    if robot_name == "":
+    if robot_namespace == "":
         return "/"
     else:
-        return "/" + robot_name + "/"
+        return "/" + robot_namespace + "/"
 
 
-def device_prefix(robot_name, device_name):
-    return robot_prefix(robot_name) + device_name + "/"
+def device_prefix(robot_namespace, device_name):
+    return robot_prefix(robot_namespace) + device_name + "/"
 
 
-def robot_urdf_prefix(robot_name):
+def robot_urdf_prefix(robot_namespace):
 
-    if robot_name == "" :
+    if robot_namespace == "" :
       return ""
     else:
-     return robot_name+"_"
+     return robot_namespace+"_"
