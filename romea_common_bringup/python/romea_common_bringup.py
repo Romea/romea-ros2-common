9df2d994de4c3d95402c3d7979f7f930f561eb6a
jean.laneurit <jean.laneurit@inrae.fr>
2022-12-18 22:45:15 +0100

    bringup : add python module unit tests

--- romea_common_bringup/python/romea_common_bringup.py
+++ romea_common_bringup/python/romea_common_bringup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import yaml
 
 
 def robot_prefix(robot_namespace):
@@ -10,12 +11,71 @@ def robot_prefix(robot_namespace):
 
 
 def device_prefix(robot_namespace, device_name):
-    return robot_prefix(robot_namespace) + device_name + "/"
+
+    if device_name == "":
+        return robot_prefix(robot_namespace)
+    else:
+        return robot_prefix(robot_namespace) + device_name + "/"
 
 
 def robot_urdf_prefix(robot_namespace):
 
-    if robot_namespace == "" :
-      return ""
+    if robot_namespace == "":
+        return ""
     else:
-     return robot_namespace+"_"
+        return robot_namespace + "_"
+
+
+class MetaDescription:
+    def __init__(self, description_type, meta_description_filename):
+
+        self.type = description_type
+        self.filename = meta_description_filename
+        with open(meta_description_filename) as f:
+            self.data = yaml.safe_load(f)
+
+    def exists(self, param, ns=None):
+        if ns:
+            return ns in self.data and param in self.data[ns]
+        else:
+            return param in self.data
+
+    def get_or(self, param, ns=None, default=None):
+
+        if ns is not None:
+            try:
+                return self.data[ns].get(param, default)
+            except:
+                raise LookupError(
+                    "Cannot get param"
+                    + self.param_name_(param, ns)
+                    + " from "
+                    + self.type
+                    + " description file "
+                    + self.filename
+                )
+        else:
+            return self.data.get(param, default)
+
+    def get(self, param, ns=None):
+
+        value = self.get_or(param, ns)
+        print("value", value)
+
+        if value is None:
+            raise LookupError(
+                "Cannot get param"
+                + self.param_name_(param, ns)
+                + " from "
+                + self.type
+                + " description file "
+                + self.filename
+            )
+
+        return value
+
+    def param_name_(self, param, ns):
+        if ns:
+            return ns + "." + param
+        else:
+            return param
---
+++ romea_common_bringup/test/test_common_bringup.py
@@ -0,0 +1,84 @@
+
+import os
+import pytest
+
+from romea_common_bringup import (
+    robot_prefix,
+    device_prefix,
+    robot_urdf_prefix,
+    MetaDescription,
+)
+
+def test_robot_prefix():
+    assert robot_prefix("")=="/"
+    assert robot_prefix("robot")=="/robot/"
+
+def test_device_prefix():
+    assert device_prefix("robot","")=="/robot/"
+    assert device_prefix("robot","device")=="/robot/device/"
+
+def test_robot_urdf_prefix():
+    assert robot_urdf_prefix("")==""
+    assert robot_urdf_prefix("robot")=="robot_"
+
+@pytest.fixture(scope="module")
+def meta_description():
+    meta_description_filename = os.path.join(os.getcwd(),"test_common_bringup.yaml")
+    return MetaDescription("common",meta_description_filename)
+
+def test_foo_exists(meta_description):
+    assert meta_description.exists("foo") == True
+
+def test_qux_baz_exists(meta_description):
+    assert meta_description.exists("qux","baz") == True
+
+def test_bar_does_not_exists(meta_description):
+    assert meta_description.exists("bar") == False
+
+def test_foo_thud_does_not_exists(meta_description):
+    assert meta_description.exists("foo","thud") == False
+
+def test_baz_foo_does_not_exists(meta_description):
+    assert meta_description.exists("baz","foo") == False
+
+def test_get_or_foo(meta_description):
+    assert meta_description.get_or("foo") == "bar"
+
+def test_get_or_qux_baz(meta_description):
+    assert meta_description.get_or("qux","baz") == "thud"
+
+def test_get_or_bar(meta_description):
+    assert meta_description.get_or("bar") == None
+
+def test_get_or_foo_baz(meta_description):
+    assert meta_description.get_or("foo","baz") == None
+
+def test_get_or_foo_thud(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+        meta_description.get_or("foo","thud")
+
+def test_get_or_baz_foo(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+       meta_description.get_or("baz","foo")
+
+def test_get_foo(meta_description):
+    assert meta_description.get("foo") == "bar"
+
+def test_get_qux_baz(meta_description):
+    assert meta_description.get("qux","baz") == "thud"
+
+def test_get_bar(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+       assert meta_description.get("bar") 
+
+def test_get_foo_baz(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+       assert meta_description.get("foo","baz")
+
+def test_get_foo_thud(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+        meta_description.get_or("foo","thud")
+
+def test_get_baz_foo(meta_description):
+    with pytest.raises(LookupError) as excinfo:
+       meta_description.get("baz","foo")
